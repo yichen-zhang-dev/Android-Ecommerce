cca7d8f2a2f81ad62fd6349d9be400c67c7a9d6e
Yichen Zhang <yichenjake@utexas.edu>
2021-09-10 16:01:49 -0400

    build a cart button and add upward navigation

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button cart_button = findViewById(R.id.cart_button);
+        cart_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Cart.class);
+                startActivity(intent);
+            }
+        });
     }
 }

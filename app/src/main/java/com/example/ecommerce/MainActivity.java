69514f17554008701172bcbd2169e01e480181a0
Yichen Zhang <yichenjake@utexas.edu>
2021-09-11 13:49:40 -0400

    Populate cities collection

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -11,29 +11,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Document;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Firebase Tutorial";
+    private static final String ECOMMERCE_TAG = "Ecommerce";
+    private static FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebase();
+        Log.d(ECOMMERCE_TAG, "======================Firing up Firebase======================");
+        db = FirebaseFirestore.getInstance();
+        populateCities();
 
+        // Start Cart activity
         final ImageButton cart_button = (ImageButton) findViewById(R.id.cart_button);
         cart_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -43,37 +55,81 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void firebase() {
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
+//    private void firebase() {
+//        // Create a new user with a first and last name
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "Yichen");
+//        user.put("last", "Zhang");
+//        user.put("born", 1999);
 //
-//        myRef.setValue("Hello, World!");
+//        DocumentReference userYichen = db.collection("users").document("Yichen");
+//
+//        // Add a new document with a generated ID
+////        db.collection("users")
+////                .add(user)
+//        userYichen.set(user)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void unused) {
+//                        Log.d(TAG, "DocumentSnapshot successfully written!");
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+//    }
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private void populateCities() {
+        CollectionReference cities = db.collection("cities");
 
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Yichen");
-        user.put("last", "Zhang");
-        user.put("born", 1999);
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("name", "San Francisco");
+        data1.put("state", "CA");
+        data1.put("country", "USA");
+        data1.put("capital", false);
+        data1.put("population", 860000);
+        data1.put("regions", Arrays.asList("west_coast", "norcal"));
+        cities.document("SF").set(data1);
 
-        DocumentReference userYichen = db.collection("users").document("Yichen");
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("name", "Los Angeles");
+        data2.put("state", "CA");
+        data2.put("country", "USA");
+        data2.put("capital", false);
+        data2.put("population", 3900000);
+        data2.put("regions", Arrays.asList("west_coast", "socal"));
+        cities.document("LA").set(data2);
 
-        // Add a new document with a generated ID
-//        db.collection("users")
-//                .add(user)
-        userYichen.set(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Log.d(TAG, "DocumentSnapshot successfully written!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        Map<String, Object> data3 = new HashMap<>();
+        data3.put("name", "Washington D.C.");
+        data3.put("state", null);
+        data3.put("country", "USA");
+        data3.put("capital", true);
+        data3.put("population", 680000);
+        data3.put("regions", Arrays.asList("east_coast"));
+        cities.document("DC").set(data3);
+
+        Map<String, Object> data4 = new HashMap<>();
+        data4.put("name", "Tokyo");
+        data4.put("state", null);
+        data4.put("country", "Japan");
+        data4.put("capital", true);
+        data4.put("population", 9000000);
+        data4.put("regions", Arrays.asList("kanto", "honshu"));
+        cities.document("TOK").set(data4);
+
+        Map<String, Object> data5 = new HashMap<>();
+        data5.put("name", "Beijing");
+        data5.put("state", null);
+        data5.put("country", "China");
+        data5.put("capital", true);
+        data5.put("population", 21500000);
+        data5.put("regions", Arrays.asList("jingjinji", "hebei"));
+        cities.document("BJ").set(data5);
+
+        Log.d(TAG, "======================Completed populating cities======================");
     }
 }
